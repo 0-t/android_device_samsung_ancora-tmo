123864227d403dfadbdb0c88896731007b62df00
Arne Coucheron <arco68@gmail.com>
2012-04-09 06:29:52 +0200

    Change ordering of inherit-product in cm.mk

--- cm.mk
+++ cm.mk
@@ -1,11 +1,13 @@
+# Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Inherit some common CM stuff.
+$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+
 # Inherit device configuration
 $(call inherit-product, $(LOCAL_PATH)/full_ancora_tmo.mk)
 
 PRODUCT_RELEASE_NAME := ancora_tmo
-
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
-$(call inherit-product, vendor/cm/config/gsm.mk)
 
 # Setup device configuration
 PRODUCT_NAME := cm_ancora_tmo
@@ -15,4 +17,3 @@ PRODUCT_MANUFACTURER := Samsung
 PRODUCT_MODEL := SGH-T679
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=samsung/SGH-T679/SGH-T679:2.3.5/GINGERBREAD/VKJ1:user/release-keys PRIVATE_BUILD_DESC="SGH-T679-user 2.3.5 GINGERBREAD VKJ1 release-keys"
-
