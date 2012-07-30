24efd6ed7ec694ffc4c7c0a432e0c50c407873d1
Arne Coucheron <arco68@gmail.com>
2012-07-30 03:11:51 +0200

    More camera HAL updates
    Fix for touch-to-focus and exposure

--- camerahal/cameraHAL.cpp
+++ camerahal/cameraHAL.cpp
@@ -22,6 +22,9 @@
 #define MAX_CAMERAS_SUPPORTED 2
 #define GRALLOC_USAGE_PMEM_PRIVATE_ADSP GRALLOC_USAGE_PRIVATE_0
 
+#define CAMERA_ID_FRONT 1
+#define CAMERA_ID_BACK 0
+
 #include <fcntl.h>
 #include <stdint.h>
 #include <string.h>
@@ -378,22 +381,46 @@ static void wrap_data_callback_timestamp(nsecs_t timestamp, int32_t msg_type,
 
 }
 
-
 /*******************************************************************
  * implementation of priv_camera_device_ops functions
  *******************************************************************/
 
-void CameraHAL_FixupParams(android::CameraParameters &camParams)
+void CameraHAL_FixupParams(android::CameraParameters &camParams,priv_camera_device_t* dev)
 {
     const char *preferred_size = "640x480";
+    const char *preview_frame_rates  = "31,30,29,28,27,26,25,24,23,22,21,20,19,18,17,16,15,14,13,12,11,10,9,8,7,6,5";
+    const char *preferred_rate = "31";
 
     camParams.set(android::CameraParameters::KEY_VIDEO_FRAME_FORMAT,
                   android::CameraParameters::PIXEL_FORMAT_YUV420SP);
 
     camParams.set(CameraParameters::KEY_PREFERRED_PREVIEW_SIZE_FOR_VIDEO,
                   preferred_size);
-}
 
+    camParams.set(android::CameraParameters::KEY_MAX_SHARPNESS, "30");
+    camParams.set(android::CameraParameters::KEY_MAX_CONTRAST, "10");
+    camParams.set(android::CameraParameters::KEY_MAX_SATURATION, "10");
+
+    if (!camParams.get(android::CameraParameters::KEY_SUPPORTED_PREVIEW_FRAME_RATES)) {
+        camParams.set(android::CameraParameters::KEY_SUPPORTED_PREVIEW_FRAME_RATES,
+                      preview_frame_rates);
+    }
+
+    if (!camParams.get(android::CameraParameters::KEY_PREVIEW_FRAME_RATE)) {
+        camParams.set(CameraParameters::KEY_PREVIEW_FRAME_RATE, preferred_rate);
+    }
+
+    if (dev->cameraid==CAMERA_ID_BACK) {
+        if (!camParams.get(android::CameraParameters::KEY_MAX_NUM_FOCUS_AREAS)) {
+            camParams.set(CameraParameters::KEY_MAX_NUM_FOCUS_AREAS, 1);
+        }
+    }
+
+    camParams.set(CameraParameters::KEY_MAX_EXPOSURE_COMPENSATION, 4);
+    camParams.set(CameraParameters::KEY_MIN_EXPOSURE_COMPENSATION, -4);
+    camParams.set(CameraParameters::KEY_EXPOSURE_COMPENSATION_STEP, 1);
+
+}
 
 int camera_set_preview_window(struct camera_device * device,
                               struct preview_stream_ops *window)
@@ -580,8 +607,8 @@ int camera_start_preview(struct camera_device * device)
 
     rv = gCameraHals[dev->cameraid]->startPreview();
     LOGI("%s--- rv %d", __FUNCTION__,rv);
-   
-    if(!rv) 
+
+    if(!rv)
       dev->preview_started=1;
 
     return rv;
@@ -809,7 +836,6 @@ int camera_set_parameters(struct camera_device * device, const char *params)
     camParams.dump();
 #endif
 
-
     rv = gCameraHals[dev->cameraid]->setParameters(camParams);
 
 #if 0
@@ -838,7 +864,7 @@ char* camera_get_parameters(struct camera_device * device)
     camParams.dump();
 #endif
 
-    CameraHAL_FixupParams(camParams);
+    CameraHAL_FixupParams(camParams,dev);
 
     camParams.set("orientation", "landscape");
 
@@ -1110,9 +1136,8 @@ int camera_get_camera_info(int camera_id, struct camera_info *info)
     CameraInfo cameraInfo;
 
     android::SEC_getCameraInfo(camera_id, &cameraInfo);
-  
+
     info->facing = cameraInfo.facing;
-    //info->orientation = cameraInfo.orientation;
     if(info->facing == 1) {
         info->orientation = 270;
     } else {
