d3d63150c666c55f73426d9072b33f8069346e9e
Arne Coucheron <arco68@gmail.com>
2012-08-19 22:58:59 +0200

    Remove BOARD_UMS_LUNFILE
    Mounting devices are taken care of automatically in latest recovery.

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -140,7 +140,6 @@ TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/ancora_tmo/recovery/recovery_keys.c
 BOARD_CUSTOM_GRAPHICS := ../../../device/samsung/ancora_tmo/recovery/graphics.c
 TARGET_RECOVERY_INITRC := device/samsung/ancora_tmo/init.recovery.rc
-BOARD_UMS_LUNFILE := /sys/devices/platform/usb_mass_storage/lun0/file
 # End recovery stuff
 
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/usb_mass_storage/lun%d/file
