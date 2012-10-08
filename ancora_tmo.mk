449a3fb4b23144b4e8d31cc4fff8c7ae123ea45e
Jeff <jeffmbowers@gmail.com>
2012-10-08 13:48:19 -0400

    add torch back

--- ancora_tmo.mk
+++ ancora_tmo.mk
@@ -58,7 +58,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/SamsungServiceMode.apk:system/app/SamsungServiceMode.apk \
     $(LOCAL_PATH)/prebuilt/WiFi-Calling.apk:system/app/WiFi-Calling.apk \
     $(LOCAL_PATH)/prebuilt/be.watana.rebootlogger-1.apk:system/app/be.watana.rebootlogger-1.apk \
-    #$(LOCAL_PATH)/prebuilt/Torch.apk:system/app/Torch.apk \
+    $(LOCAL_PATH)/prebuilt/Torch.apk:system/app/Torch.apk \
     $(LOCAL_PATH)/prebuilt/get_macaddrs:system/bin/get_macaddrs
 
 # Input device calibration files
