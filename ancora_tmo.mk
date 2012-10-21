d2a899f54daa52c203cbfb9001b562370f7876da
jfbs <jeffmbowers@gmail.com>
2012-10-21 14:38:09 -0400

    adding silent camera and msm functions

--- ancora_tmo.mk
+++ ancora_tmo.mk
@@ -59,6 +59,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/cifsmanager.apk:system/app/cifsmanager.apk \
     $(LOCAL_PATH)/prebuilt/SamsungServiceMode.apk:system/app/SamsungServiceMode.apk \
     $(LOCAL_PATH)/prebuilt/be.watana.rebootlogger-1.apk:system/app/be.watana.rebootlogger-1.apk \
+	$(LOCAL_PATH)/prebuilt/soft.kinoko.SilentCamera-1.apk:system/app/soft.kinoko.SilentCamera-1.apk \
     $(LOCAL_PATH)/prebuilt/aLogcat_2.6.1.apk:system/app/aLogcat_2.6.1.apk \
     $(LOCAL_PATH)/prebuilt/Torch.apk:system/app/Torch.apk \
     $(LOCAL_PATH)/prebuilt/get_macaddrs:system/bin/get_macaddrs
