dbebafdde5d3f87836ccca4f520445ba56110219
jfbs <jeffmbowers@gmail.com>
2012-11-14 08:43:40 -0500

    Fix typo

--- camerahal/Android.mk
+++ camerahal/Android.mk
@@ -18,7 +18,7 @@ LOCAL_SRC_FILES := cameraHAL.cpp
 LOCAL_C_INCLUDES := $(TOP)/frameworks/native/include
 
 LOCAL_SHARED_LIBRARIES := liblog libutils libcutils
-LOCAL_SHARED_LIBRARIES := libseccameraadaptor libcamera
+LOCAL_SHARED_LIBRARIES += libseccameraadaptor libcamera
 LOCAL_SHARED_LIBRARIES += libui libhardware libcamera_client
 LOCAL_PRELINK_MODULE := false
 
