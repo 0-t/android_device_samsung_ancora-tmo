d960802fee9958c34475962392c962bf5f7e1ec1
Arne Coucheron <arco68@gmail.com>
2012-11-16 04:34:28 +0100

    Build camera HAL

--- camerahal/Android.mk
+++ camerahal/Android.mk
@@ -1,5 +1,3 @@
-ifeq ($(BOARD_USES_LEGACY_CAMERA),true)
-
 $(shell mkdir -p $(OUT)/obj/SHARED_LIBRARIES/libcamera_intermediates/)
 $(shell touch $(OUT)/obj/SHARED_LIBRARIES/libcamera_intermediates/export_includes)
 $(shell mkdir -p $(OUT)/obj/SHARED_LIBRARIES/libseccameraadaptor_intermediates/)
@@ -23,5 +21,3 @@ LOCAL_SHARED_LIBRARIES += libui libhardware libseccameraadaptor
 LOCAL_PRELINK_MODULE := false
 
 include $(BUILD_SHARED_LIBRARY)
-
-endif
