afc2eefa07e30e259e9dfb4e297e07974fa89c57
jfbs <jeffmbowers@gmail.com>
2013-02-01 19:05:56 -0500

    Revert "Revert "Remove Torch app for now""
    This reverts commit b8d08b55dadde17d2f6a3aa8d26b378f5ac5196e.

--- ancora_tmo.mk
+++ ancora_tmo.mk
@@ -52,8 +52,11 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/config/vold.fstab:system/etc/vold.fstab \
     $(LOCAL_PATH)/config/fstab.qcom:root/fstab.qcom \
     $(LOCAL_PATH)/config/nvram_net.txt:system/vendor/firmware/nvram_net.txt \
-    $(LOCAL_PATH)/prebuilt/get_macaddrs:system/bin/get_macaddrs \
-    $(LOCAL_PATH)/prebuilt/Torch.apk:system/app/Torch.apk
+    $(LOCAL_PATH)/prebuilt/com.estrongs.android.pop.apk:system/app/com.estrongs.android.pop.apk \
+    $(LOCAL_PATH)/prebuilt/cifsmanager.apk:system/app/cifsmanager.apk \
+    $(LOCAL_PATH)/prebuilt/SamsungServiceMode.apk:system/app/SamsungServiceMode.apk \
+    $(LOCAL_PATH)/prebuilt/aLogcat_2.6.1.apk:system/app/aLogcat_2.6.1.apk \
+    $(LOCAL_PATH)/prebuilt/get_macaddrs:system/bin/get_macaddrs
 
 # $(LOCAL_PATH)/prebuilt/WiFi-Calling.apk:system/app/WiFi-Calling.apk \
 
