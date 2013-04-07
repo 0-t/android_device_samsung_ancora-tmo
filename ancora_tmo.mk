96e536c3f9a251c960c6000fa2e6442369782033
jfbs <jeffmbowers@gmail.com>
2013-04-07 10:27:22 -0400

    Update prebuilts

--- ancora_tmo.mk
+++ ancora_tmo.mk
@@ -78,9 +78,7 @@ PRODUCT_COPY_FILES += \
 # Modules
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/dhd.ko:root/lib/modules/dhd.ko \
-    $(LOCAL_PATH)/prebuilt/cifs.ko:root/lib/modules/cifs.ko \
-    $(LOCAL_PATH)/prebuilt/dhd.ko:system/lib/modules/dhd.ko \
-    $(LOCAL_PATH)/prebuilt/cifs.ko:system/lib/modules/cifs.ko
+    $(LOCAL_PATH)/prebuilt/cifs.ko:root/lib/modules/cifs.ko
 
 # LPM
 PRODUCT_COPY_FILES += \
