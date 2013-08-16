6ab0696b69617fce3dc35e1c11c13ce03077bfff
jfbs <jeffmbowers@gmail.com>
2013-08-16 17:11:30 -0400

    Add torch

--- ancora_tmo.mk
+++ ancora_tmo.mk
@@ -54,7 +54,8 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/prebuilt/com.estrongs.android.pop.apk:system/app/com.estrongs.android.pop.apk \
     $(LOCAL_PATH)/prebuilt/SamsungServiceMode.apk:system/app/SamsungServiceMode.apk \
     $(LOCAL_PATH)/prebuilt/aLogcat_2.6.1.apk:system/app/aLogcat_2.6.1.apk \
-    $(LOCAL_PATH)/prebuilt/Flashlight_4.9.4.apk:system/app/Flashlight_4.9.4.apk
+    $(LOCAL_PATH)/prebuilt/Flashlight_4.9.4.apk:system/app/Flashlight_4.9.4.apk \
+    $(LOCAL_PATH)/prebuilt/Torch.apk:system/app/Torch.apk
 
 # Needed to reset bootmode when leaving recovery
 PRODUCT_COPY_FILES += \
