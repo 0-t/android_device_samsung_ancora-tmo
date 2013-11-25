68acfc979b209124eb1288bcb8064a7e0073ba4f
jfbs <jeffmbowers@gmail.com>
2013-11-24 23:13:02 -0500

    Revert "Try memtrack"
    This reverts commit a75a0135adf281d6d36b4e2d6d035536deb38105.

--- ancora_tmo.mk
+++ ancora_tmo.mk
@@ -112,7 +112,6 @@ PRODUCT_PACKAGES += \
     gps.msm7x30 \
     lights.msm7x30 \
     power.msm7x30 \
-    memtrack.msm7x30 \
     audio.primary.msm7x30 \
     audio_policy.msm7x30 \
     audio.a2dp.default \
