81b4d19d1e00193bb1a34bd9627aa7eeb971b261
jfbs <jeffmbowers@gmail.com>
2013-12-04 17:53:16 -0500

    add twrp recovery picked from ADC-Team

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -175,6 +175,26 @@ TARGET_RECOVERY_FSTAB := device/samsung/ancora_tmo/config/fstab.qcom
 TARGET_KERNEL_SOURCE := kernel/samsung/ancora_tmo
 TARGET_KERNEL_CONFIG := ancora_tmo_defconfig
 
+# TWRP recovery stuff
+BOARD_HAS_SDCARD_INTERNAL := true
+BOARD_USES_MMCUTILS := true
+BOARD_HAS_NO_MISC_PARTITION := true
+BOARD_HAS_NO_SELECT_BUTTON := true
+DEVICE_RESOLUTION := 480x800
+TW_BRIGHTNESS_PATH := /sys/devices/platform/msm_fb.196609/leds/lcd-backlight/brightness
+TW_MAX_BRIGHTNESS := 255
+TW_CUSTOM_POWER_BUTTON := 107
+BOARD_USE_CUSTOM_RECOVERY_FONT:= \"roboto_10x18.h\"
+TW_INTERNAL_STORAGE_PATH := "/sdcard"
+TW_INTERNAL_STORAGE_MOUNT_POINT := "sdcard"
+TW_EXTERNAL_STORAGE_PATH := "/external_sd"
+TW_EXTERNAL_STORAGE_MOUNT_POINT := "external_sd"
+TW_NO_REBOOT_BOOTLOADER := true
+TW_HAS_DOWNLOAD_MODE := true
+TW_USE_MODEL_HARDWARE_ID_FOR_DEVICE_ID := true
+TW_INCLUDE_FB2PNG := true
+TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
+
 BOARD_KERNEL_CMDLINE := androidboot.selinux=permissive
 
 # SELinux
