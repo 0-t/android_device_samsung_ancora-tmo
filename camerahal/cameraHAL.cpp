ccb970cabab429cdb172a714bfc7e422fb6a487e
jfbs <jeffmbowers@gmail.com>
2013-12-09 16:56:39 -0500

    camerahal: update rear camera

--- camerahal/cameraHAL.cpp
+++ camerahal/cameraHAL.cpp
@@ -421,6 +421,12 @@ void CameraHAL_FixupParams(android::CameraParameters &camParams, priv_camera_dev
         if (!camParams.get(android::CameraParameters::KEY_MAX_NUM_FOCUS_AREAS)) {
             camParams.set(CameraParameters::KEY_MAX_NUM_FOCUS_AREAS, 1);
         }
+        camParams.set(CameraParameters::KEY_SUPPORTED_FOCUS_MODES, "off");
+        camParams.set(CameraParameters::KEY_SUPPORTED_ISO_MODES, "auto,ISO50,ISO100,ISO200,ISO400");
+
+        camParams.set(CameraParameters::KEY_ZOOM_SUPPORTED, CameraParameters::FALSE);
+
+        camParams.set(CameraParameters::KEY_SUPPORTED_PREVIEW_FRAME_RATES, "30");
     }
 
     camParams.set(CameraParameters::KEY_MAX_EXPOSURE_COMPENSATION, 4);
