e2a62d914770a9987710e49853a553c76dc3c599
jfbs <jeffmbowers@gmail.com>
2013-12-18 09:56:21 -0500

    ancora_tmo: set low_ram back to true

--- ancora_tmo.mk
+++ ancora_tmo.mk
@@ -165,7 +165,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 # For applications to determine if they should turn off specific memory-intensive
 # features that work poorly on low-memory devices.
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.low_ram=false
+    ro.config.low_ram=true
 
 # Fix legacy fb on kitkat
 PRODUCT_PROPERTY_OVERRIDES += \
