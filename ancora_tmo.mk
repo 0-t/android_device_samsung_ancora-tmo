4eba56b6fc2c8109c00ff21e1449ff1b405063de
jfbs <jeffmbowers@gmail.com>
2013-12-24 01:50:02 -0500

    Revert "ancora_tmo: temp. remove zram - needs kernel module"
    This reverts commit e52c62b1ece9ae884051daf246b91ac9c33c8ed6.
    
    Conflicts:
    
    	ancora_tmo.mk

--- ancora_tmo.mk
+++ ancora_tmo.mk
@@ -158,6 +158,14 @@ PRODUCT_PACKAGES += \
 # Torch
 # PRODUCT_PACKAGES += Torch
 
+# Enable zRAM by default
+PRODUCT_PROPERTY_OVERRIDES += \
+	ro.zram.default=18
+
+# Fix screenshots with legacy FB
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.bq.gpu_to_cpu_unsupported=1
+
 # For applications to determine if they should turn off specific memory-intensive
 # features that work poorly on low-memory devices.
 PRODUCT_PROPERTY_OVERRIDES += \
