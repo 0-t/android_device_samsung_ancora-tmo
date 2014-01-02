b913603766dda19807fd73163dc74df49fa074a7
jfbs <jeffmbowers@gmail.com>
2014-01-01 23:41:18 -0500

    ancora_tmo: enable low_ram

--- ancora_tmo.mk
+++ ancora_tmo.mk
@@ -163,7 +163,7 @@ PRODUCT_PACKAGES += \
 # For applications to determine if they should turn off specific memory-intensive
 # features that work poorly on low-memory devices.
 PRODUCT_PROPERTY_OVERRIDES += \
-    ro.config.low_ram=false
+    ro.config.low_ram=true
 
 PRODUCT_PACKAGES += \
     libexifa \
