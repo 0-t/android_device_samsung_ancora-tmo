46790eabf4c45e7863651184f5567c2a708e1b55
jfbs <jeffmbowers@gmail.com>
2014-01-15 20:38:09 -0500

    remove SamsungServiceMode

--- ancora_tmo.mk
+++ ancora_tmo.mk
@@ -49,8 +49,7 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/config/fstab.qcom:root/fstab.qcom \
     $(LOCAL_PATH)/config/nvram_net.txt:system/vendor/firmware/nvram_net.txt \
     $(LOCAL_PATH)/prebuilt/get_macaddrs:system/bin/get_macaddrs \
-    $(LOCAL_PATH)/prebuilt/ES_File_Explorer_3.0.8.0.apk:system/app/ES_File_Explorer_3.0.8.0.apk \
-    $(LOCAL_PATH)/prebuilt/SamsungServiceMode.apk:system/app/SamsungServiceMode.apk
+    $(LOCAL_PATH)/prebuilt/ES_File_Explorer_3.0.8.0.apk:system/app/ES_File_Explorer_3.0.8.0.apk
 
 # Needed to reset bootmode when leaving recovery
 PRODUCT_COPY_FILES += \
