4c7f7875b0339729a5b742bfe9597f8a441a14ff
jfbs <jeffmbowers@gmail.com>
2014-02-16 22:30:37 -0500

    disable AAC encoder
    picked from: https://github.com/CyanogenMod/android_device_samsung_qcom-common/commit/67474c1da5f9afa89ef39b76b1ba8a20dd297ae8

--- ancora_tmo.mk
+++ ancora_tmo.mk
@@ -175,10 +175,6 @@ ADDITIONAL_DEFAULT_PROPERTIES += \
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.ksm.default=1
 
-# AAC encoder
-PRODUCT_PROPERTY_OVERRIDES += \
-    qcom.hw.aac.encoder=true
-
 # Network
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.locationfeatures=1 \
