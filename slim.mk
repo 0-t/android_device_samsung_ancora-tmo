79ac21d0e29b9be748b6ae2057c6fb5ecf857ed1
jfbs <phrewn@gmail.com>
2014-04-18 00:40:25 -0400

    slim init

--- slim.mk
+++ slim.mk
@@ -1,5 +1,5 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common slim stuff.
+$(call inherit-product, vendor/slim/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, $(LOCAL_PATH)/full_ancora_tmo.mk)
@@ -7,7 +7,7 @@ $(call inherit-product, $(LOCAL_PATH)/full_ancora_tmo.mk)
 PRODUCT_RELEASE_NAME := ancora_tmo
 
 # Setup device configuration
-PRODUCT_NAME := cm_ancora_tmo
+PRODUCT_NAME := slim_ancora_tmo
 PRODUCT_DEVICE := ancora_tmo
 PRODUCT_BRAND := Samsung
 PRODUCT_MANUFACTURER := Samsung
