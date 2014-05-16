4ec9637d0a4b1772b9e6d39acb697895cea1671f
jfbs <phrewn@gmail.com>
2014-05-16 00:33:06 -0400

    build torch
    Ported from: https://github.com/Blefish/android_device_huawei_u8800/commit/af008017c2d845334a9b9d8f640e56f147600329

--- ancora_tmo.mk
+++ ancora_tmo.mk
@@ -163,7 +163,8 @@ PRODUCT_PACKAGES += \
 
 # Misc
 PRODUCT_PACKAGES += \
-    SamsungServiceMode
+    SamsungServiceMode \
+    Torch
 
 # For applications to determine if they should turn off specific memory-intensive
 # features that work poorly on low-memory devices.
