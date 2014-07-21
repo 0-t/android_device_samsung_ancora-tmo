9b77ba887109c17af88bcecdf63dcd576ad96ddb
jfbs <phrewn@gmail.com>
2014-07-20 22:35:49 -0400

    mahdi init

--- mahdi.mk
+++ mahdi.mk
@@ -1,5 +1,6 @@
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common Mahdi stuff.
+$(call inherit-product, vendor/mahdi/configs/common.mk)
+$(call inherit-product, vendor/mahdi/config/common_full_phone.mk)
 
 # Inherit device configuration
 $(call inherit-product, $(LOCAL_PATH)/full_ancora_tmo.mk)
@@ -7,10 +8,14 @@ $(call inherit-product, $(LOCAL_PATH)/full_ancora_tmo.mk)
 PRODUCT_RELEASE_NAME := ancora_tmo
 
 # Setup device configuration
-PRODUCT_NAME := cm_ancora_tmo
+PRODUCT_NAME := mahdi_ancora_tmo
 PRODUCT_DEVICE := ancora_tmo
 PRODUCT_BRAND := Samsung
 PRODUCT_MANUFACTURER := Samsung
 PRODUCT_MODEL := SGH-T679
 
 PRODUCT_BUILD_PROP_OVERRIDES += BUILD_FINGERPRINT=samsung/SGH-T679/SGH-T679:2.3.6/GINGERBREAD/UVLE1:user/release-keys PRIVATE_BUILD_DESC="SGH-T679-user 2.3.6 GINGERBREAD UVLE1 release-keys"
+
+# Copy device specific prebuilt files.
+PRODUCT_COPY_FILES += \
+ vendor/mahdi/prebuilt/bootanimations/BOOTANIMATION-800x480.zip:system/media/bootanimation.zip
--- vendorsetup.sh
+++ vendorsetup.sh
@@ -18,4 +18,4 @@
 # In particular, you can add lunch options with the add_lunch_combo
 # function: add_lunch_combo generic-eng
 
-add_lunch_combo cm_ancora_tmo-userdebug
+add_lunch_combo mahdi_ancora_tmo-userdebug
