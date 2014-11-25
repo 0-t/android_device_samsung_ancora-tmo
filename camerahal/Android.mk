ad75b7b823e0427a1b737b311fe24b806b6a52d1
Cristoforo Cataldo <cristoforo.cataldo@gmail.com>
2014-11-25 11:38:44 +0100

    camera: Update include paths
    Conflicts:
    	camerahal/Android.mk

---
+++ camerahal/Android.mk
@@ -0,0 +1,40 @@
+$(shell mkdir -p $(OUT)/obj/SHARED_LIBRARIES/libcamera_intermediates/)
+$(shell touch $(OUT)/obj/SHARED_LIBRARIES/libcamera_intermediates/export_includes)
+$(shell mkdir -p $(OUT)/obj/SHARED_LIBRARIES/libseccameraadaptor_intermediates/)
+$(shell touch $(OUT)/obj/SHARED_LIBRARIES/libseccameraadaptor_intermediates/export_includes)
+
+LOCAL_PATH:= $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_MODULE_PATH := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE := camera.$(TARGET_BOARD_PLATFORM)
+
+LOCAL_MODULE_TAGS := optional
+
+LOCAL_SRC_FILES := cameraHAL.cpp \
+	exif/exif.c \
+	exif/gpsinfo.c \
+	exif/iptc.c \
+	exif/jhead.c \
+	exif/jpgfile.c \
+	exif/makernote.c
+                
+LOCAL_C_INCLUDES := frameworks/av/include
+LOCAL_C_INCLUDES += frameworks/native/include
+LOCAL_C_INCLUDES += system/media/camera/include
+LOCAL_C_INCLUDES += $(call project-path-for,qcom-display)/libgralloc
+
+LOCAL_SHARED_LIBRARIES := liblog libutils libcutils libbinder
+LOCAL_SHARED_LIBRARIES += libui libhardware libcamera_client
+LOCAL_SHARED_LIBRARIES += libseccameraadaptor
+LOCAL_PRELINK_MODULE := false
+
+
+# hack for prebuilt
+$(shell mkdir -p $(OUT)/obj/SHARED_LIBRARIES/libcamera_intermediates/)
+$(shell touch $(OUT)/obj/SHARED_LIBRARIES/libcamera_intermediates/export_includes)
+$(shell mkdir -p $(OUT)/obj/SHARED_LIBRARIES/libseccameraadaptor_intermediates/)
+$(shell touch $(OUT)/obj/SHARED_LIBRARIES/libseccameraadaptor_intermediates/export_includes)
+
+include $(BUILD_SHARED_LIBRARY)
