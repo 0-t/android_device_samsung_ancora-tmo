arco/android_device_samsung_ancora

Various updates and cleanups

BoardConfig.mk
@@ -47,7 +47,7 @@ TARGET_ARCH_LOWMEM := true
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00400000
-BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_PAGESIZE := 2048
 TARGET_KERNEL_SOURCE := kernel/samsung/msm7x30-common
 TARGET_KERNEL_CONFIG := ancora_defconfig
 
@@ -134,11 +134,12 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 7864320
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 560988160
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 1163919360
 BOARD_CACHEIMAGE_PARTITION_SIZE := 62914560
-BOARD_FLASH_BLOCK_SIZE := 4096
+BOARD_FLASH_BLOCK_SIZE := 131072
 BOARD_VOLD_EMMC_SHARES_DEV_MAJOR := true
 BOARD_VOLD_MAX_PARTITIONS := 28
 
 # Recovery
+TARGET_RECOVERY_DEVICE_DIRS += device/samsung/ancora
 BOARD_USES_MMCUTILS := true
 BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_HAS_SDCARD_INTERNAL := true

0-t/android_device_samsung_ancora-tmo

Conflicts:
	BoardConfig.mk
	ancora_tmo.mk

BoardConfig.mk
@@ -54,7 +54,7 @@ TARGET_SCREEN_WIDTH := 480
 
 # Kernel
 BOARD_KERNEL_BASE := 0x00400000
-BOARD_KERNEL_PAGESIZE := 4096
+BOARD_KERNEL_PAGESIZE := 2048
 TARGET_KERNEL_CONFIG := ancora_tmo_defconfig
 TARGET_KERNEL_SOURCE := kernel/samsung/ancora_tmo
 
@@ -153,8 +153,8 @@ BOARD_RECOVERYIMAGE_PARTITION_SIZE := 7864320
 BOARD_SYSTEMIMAGE_PARTITION_SIZE := 560988160
 BOARD_USERDATAIMAGE_PARTITION_SIZE := 1163919360
 BOARD_CACHEIMAGE_PARTITION_SIZE := 62914560
-BOARD_FLASH_BLOCK_SIZE := 4096
-
+BOARD_FLASH_BLOCK_SIZE := 131072
+TARGET_RECOVERY_DEVICE_DIRS += device/samsung/ancora_tmo
 BOARD_USES_MMCUTILS := true
 BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_HAS_SDCARD_INTERNAL := true