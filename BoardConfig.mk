arco/android_device_samsung_ancora

Various reorganizations
* Organize files a little more like other new devices

BoardConfig.mk
@@ -101,7 +101,7 @@ USE_OPENGL_RENDERER := true
 TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
 TARGET_USES_QCOM_BSP := true
 BOARD_USES_QCOM_HARDWARE := true
-BOARD_EGL_CFG := device/samsung/ancora/config/egl.cfg
+BOARD_EGL_CFG := device/samsung/ancora/configs/egl.cfg
 COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE
 COMMON_GLOBAL_CFLAGS += -DQCOM_BSP
 
@@ -143,8 +143,8 @@ BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_HAS_SDCARD_INTERNAL := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/ancora/recovery/recovery_keys.c
-TARGET_RECOVERY_INITRC := device/samsung/ancora/config/init.recovery.rc
-TARGET_RECOVERY_FSTAB := device/samsung/ancora/config/fstab.qcom
+TARGET_RECOVERY_INITRC := device/samsung/ancora/rootdir/init.recovery.rc
+TARGET_RECOVERY_FSTAB := device/samsung/ancora/rootdir/fstab.qcom
 TARGET_USERIMAGES_USE_EXT4 := true
 TARGET_USE_CUSTOM_LUN_FILE_PATH := /sys/devices/platform/msm_hsusb/gadget/lun%d/file
 COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD

0-t/android_device_samsung_ancora-tmo

Conflicts:
	BoardConfig.mk
	ancora_tmo.mk

BoardConfig.mk
@@ -91,7 +91,7 @@ BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := msm7x30
 BOARD_VENDOR_QCOM_GPS_LOC_API_AMSS_VERSION := 50000
 
 # Graphics
-BOARD_EGL_CFG := device/samsung/ancora_tmo/config/egl.cfg
+BOARD_EGL_CFG := device/samsung/ancora_tmo/configs/egl.cfg
 TARGET_USES_C2D_COMPOSITION := true
 USE_OPENGL_RENDERER := true
 
@@ -160,8 +160,8 @@ BOARD_HAS_NO_MISC_PARTITION := true
 BOARD_HAS_SDCARD_INTERNAL := true
 TARGET_RECOVERY_PIXEL_FORMAT := "RGBX_8888"
 BOARD_CUSTOM_RECOVERY_KEYMAPPING := ../../device/samsung/ancora_tmo/recovery/recovery_keys.c
-TARGET_RECOVERY_INITRC := device/samsung/ancora_tmo/config/init.recovery.rc
-TARGET_RECOVERY_FSTAB := device/samsung/ancora_tmo/config/fstab.qcom
+TARGET_RECOVERY_INITRC := device/samsung/ancora_tmo/rootdir/init.recovery.rc
+TARGET_RECOVERY_FSTAB := device/samsung/ancora_tmo/rootdir/fstab.qcom
 COMMON_GLOBAL_CFLAGS += -DNO_SECURE_DISCARD
 # End recovery stuff
 