AriesVE-DevCon-TEAM/android_device_samsung_ariesve

camera: Updates to be compatible with all the used camera parameters
Thanks also @arco

BoardConfig.mk
@@ -27,6 +27,8 @@
 # Vendor stuff
 -include vendor/samsung/ariesve/BoardConfigVendor.mk
 
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/ariesve/include
+
 # Platform
 TARGET_BOOTLOADER_BOARD_NAME := ariesve
 TARGET_OTA_ASSERT_DEVICE := ariesve,GT-I9001

0-t/android_device_samsung_ancora-tmo

Conflicts:
	camera/cameraHAL.cpp

BoardConfig.mk
@@ -27,6 +27,8 @@
 # inherit from the proprietary version
 -include vendor/samsung/ancora_tmo/BoardConfigVendor.mk
 
+TARGET_SPECIFIC_HEADER_PATH := device/samsung/ancora_tmo/include
+
 # Platform
 TARGET_BOOTLOADER_BOARD_NAME := ancora_tmo
 TARGET_OTA_ASSERT_DEVICE := ancora_tmo,SGH-T679,ancora