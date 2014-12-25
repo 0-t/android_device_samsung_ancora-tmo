4f52fe55f5996ae14aa7e36e325638a31f0922f1
0-t <jeffmbowers@gmail.com>
2014-12-24 21:43:57 -0500

    remove trailing '\'

--- ancora_tmo.mk
+++ ancora_tmo.mk
@@ -50,7 +50,7 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_ffmpeg.xml:system/etc/media_codecs_ffmpeg.xml
 
 # Audio configuration
 PRODUCT_COPY_FILES += \
